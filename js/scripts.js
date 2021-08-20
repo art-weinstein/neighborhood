05d6fb8bf55032c5a2bf6b2105dc6ecd757fd9ff
Art Weinstein <artur.weinstein@gmail.com>
2021-08-20 14:06:29 -0700

    eliminated helper functions and converted number to string

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,48 @@
 // Business Logic
 
 function checkOne(number){
-    while(number >= 0) {
-    if(number.includes(1)){
-      return "beep!";
-    } else {
-      return number;
-    }
+  if(number.includes(1)){
+    return "beep!";
+  } else {
+    return number;
   }
 }
 
+
 function checkTwo(number){
-  while(number >= 0) {
-    if(number.includes(2)){
-      return "boop!";
-    } else {
-      return number;
-    }
+  if(number.includes(2)){
+    return "boop!";
+  } else {
+    return number;
   }
 }
+
 
 
 function checkThree(number){
-  while(number >= 0) {
-    if(number.includes(3)){
-      return "Won't you be my neighbor?";
-    } else {
-      return number;
-    }
+  if(number.includes(3)){
+    return "Won't you be my neighbor?";
+  } else {
+    return number;
   }
 }
+
 
 function numberPriority(number){
   let numArray = number;
   let finalArray = [];
-    if (numArray.includes("3")){
-      const three = checkThree(numArray);
-      finalArray.push(three);
-    } else if (numArray.includes("2")) {
-      const two = checkTwo(numArray);
-      finalArray.push(two);
-    } else if (numArray.includes(1)) {
-      const one = checkOne(numArray);
-      finalArray.push(one);
-    } else {
-      finalArray.push(numArray);
-    }
-    return finalArray;
+  let count = number.split(" ");
+      if (numArray.includes("3")){
+        const three = checkThree(numArray);
+        finalArray.push(three);
+      } else if (numArray.includes("2")) {
+        const two = checkTwo(numArray);
+        finalArray.push(two);
+      } else if (numArray.includes(1)) {
+        const one = checkOne(numArray);
+        finalArray.push(one);
+      } else {
+        finalArray.push(numArray);
+      }
+      return finalArray;
   }
-
-function count(number) {
-  
-}
